8c9f8a126d8431f3168c1e3bcfaf6ec06ab69ba5
DmIrina <dmytriieva.iryna@gmail.com>
2022-04-27 00:44:05 -0700

    adaptor changes

---
+++ src/AdapterPattern/WebClient.java
@@ -0,0 +1,25 @@
+package AdapterPattern;
+
+public class WebClient {
+    private WebRequester webRequester;
+
+    public WebClient(WebRequester webRequester) {
+        this.webRequester = webRequester;
+    }
+
+    private Object makeObject() {
+        //...
+        return null;
+    } // Make an Object
+
+    public void doWork() {
+        Object object = makeObject();
+        int status = webRequester.request(object);
+        if (status == 200) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Not OK");
+        }
+        return;
+    }
+}
--- src/AdapterPattern/WebService.java
+++ src/AdapterPattern/WebService.java
@@ -1,6 +1,12 @@
 package AdapterPattern;
 
 public class WebService {
+    String webhost;
+
+    public WebService(String webhost){
+        this.webhost = webhost;
+    }
+
     public Json request(Json json) {
         return json;
     }
