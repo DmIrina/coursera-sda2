d2be18881dc4c0ad3d898928fb011118d1c68ada
DmIrina <dmytriieva.iryna@gmail.com>
2022-05-01 18:44:43 -0700

    template

---
+++ src/TemplatePattern/SpaghettiMeatballs.java
@@ -0,0 +1,23 @@
+package TemplatePattern;
+
+public class SpaghettiMeatballs extends PastaDish{
+    @Override
+    void addPasta() {
+        System.out.println("Add spaghetti");
+    }
+
+    @Override
+    void addSauce() {
+        System.out.println("Add tomato sauce");
+    }
+
+    @Override
+    void addProtein() {
+        System.out.println("Add meatballs");
+    }
+
+    @Override
+    void addGarnish() {
+        System.out.println("Add Parmesan cheese");
+    }
+}
